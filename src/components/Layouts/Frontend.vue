d9b4807d1702851e37a8097971cf1ef0b30bcd71
Developer-Nasim <ajnasim72@gmail.coom>
2024-01-21 18:33:59 +0600

    changed footer impoport name

--- src/components/Layouts/Frontend.vue
+++ src/components/Layouts/Frontend.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Header from '../Others/Header/Header.vue'
-import Footer from '../Others/footer/Index.vue'
+import Footer from '../Others/Footer/Index.vue'
 </script>
 <template>
     <Header/>
